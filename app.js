74f959c063fa60e0bde3fa87a90bbac2a85956f1
ivy.houang <ivy.houang@gmail.com>
2024-08-27 14:40:32 -0400

    make it pretty

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const typeRouter = require("./routes/typeRouter");
 const volumeRouter = require("./routes/volumeRouter");
 const backpackRouter = require("./routes/backpackRouter");
 
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
+
 app.use(express.urlencoded({ extended: true }));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
